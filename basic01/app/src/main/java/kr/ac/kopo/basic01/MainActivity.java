7f5c49fd8d5a2c1d8128b833e19af0c720e32363
Shifty <jjs5827852@naver.com>
2025-03-31 15:21:35 +0900

    0331Javatext

--- basic01/app/src/main/java/kr/ac/kopo/basic01/MainActivity.java
+++ basic01/app/src/main/java/kr/ac/kopo/basic01/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.kopo.basic01;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main2);
+
+        TextView text1 = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.text2);
+        TextView text3 = findViewById(R.id.text3);
+
+        text1.setTextSize(20);
+        text1.setTextColor(Color.GREEN);
+        text1.setText("한국폴리텍대학");
+
+        text2.setTextSize(30);
+        text2.setTextColor(Color.GRAY);
+        text2.setText("인공지능소프트웨어과");
+
+        text3.setTextColor(Color.BLUE);
+        text3.setText("싱글라인싱글라인 소옥성소옥성소옥성싱글라인싱글라인 소옥성소옥성소옥성싱글라인싱글라인 소옥성소옥성소옥성");
+        text3.setSingleLine();
     }
 }
