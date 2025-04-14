ba29c16de476c71e341a3039ae6bcc8b0afef949
Shifty <jjs5827852@naver.com>
2025-04-14 15:43:40 +0900

    0414

--- showpetpics/app/src/main/java/kr/ac/kopo/showpetpics/MainActivity.java
+++ showpetpics/app/src/main/java/kr/ac/kopo/showpetpics/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.kopo.showpetpics;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -32,22 +33,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        checkStart.findViewById(R.id.check_start);
-        ls.findViewById(R.id.linear_sub);
-        rg.findViewById(R.id.rg_1);
-        rbp.findViewById(R.id.rb_1);
-        rbm.findViewById(R.id.rb_2);
-        rbc.findViewById(R.id.rb_3);
-        btn_1.findViewById(R.id.btn_1);
-        img.findViewById(R.id.img_1);
+        checkStart = findViewById(R.id.check_start);
+        ls = findViewById(R.id.linear_sub);
+        rg = findViewById(R.id.rg_1);
+        rbp = findViewById(R.id.rb_1);
+        rbm = findViewById(R.id.rb_2);
+        rbc = findViewById(R.id.rb_3);
+        btn_1 = findViewById(R.id.btn_1);
+        img = findViewById(R.id.img_1);
 
+        ls.setVisibility(View.INVISIBLE);
         checkStart.setOnCheckedChangeListener(checkStartListener);
+        btn_1.setOnClickListener(btn_1Listener);
 
     }
     CompoundButton.OnCheckedChangeListener checkStartListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+            if(isChecked){
+                ls.setVisibility(View.VISIBLE);
+            }
+            else{
+                ls.setVisibility(View.INVISIBLE);
+            }
         }
     };
+    View.OnClickListener btn_1Listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int selectedrbId = rg.getCheckedRadioButtonId();
+            if(selectedrbId==R.id.rb_1){
+                img.setImageResource(R.drawable.amlfu);
+            }
+            else if(selectedrbId==R.id.rb_2){
+                img.setImageResource(R.drawable.cat);
+            }
+            else{
+                img.setImageResource(R.drawable.malt);
+            }
+        }
+    };
+
 }
